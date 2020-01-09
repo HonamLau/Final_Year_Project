2da80b6f9ea5c93c394c6bad8d651a9d890d44a8
Pui Hong Sze <phsze@connect.ust.hk>
2020-01-09 18:30:48 +0800

    1234

--- arduino.ino
+++ arduino.ino
@@ -1,13 +1,27 @@
 #include "arduino_secret.h"
 #include <WiFiNINA.h>
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
+char ssid[] = "Mok";
+char pass[] = "1234567890";
 int status = WL_IDLE_STATUS;
 
 void setup(){
+    pinMode(13,OUTPUT);
     while (status != WL_AP_CONNECTED)
     {
-        /* code */
+        /* code *///wekuHEJKH
+        //ASDFADSFDSAFDSAFADSFDASFDASFDSAFDASFADSF
+        status=WiFi.begin(ssid,pass);
+       
+
     }
-    
+    //WiFi.begin(ssid,pass);
+}
+
+void loop(){
+
+digitalWrite(13,HIGH);
+delay(1000);
+digitalWrite(13,LOW);
+delay(1000);
+
 }
