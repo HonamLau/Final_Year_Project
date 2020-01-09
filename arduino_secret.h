19c9269fbff47a5ea3fd31036ec112a083636a95
HonamLau <hnlau@connect.ust.hk>
2020-01-09 18:25:01 +0800

    wifi

--- arduino_secret.h
+++
@@ -1,2 +0,0 @@
-#define SECRET_SSID "edurom"
-#define SECRET_PASS "1234" 
--- src/server.js
+++ src/server.js
@@ -2,3 +2,8 @@ var http = require("http");
 var app = require('express')();
 var appWs = require('express-ws')(app);
 
+app.ws('/echo', ws => {
+    ws.on('message')
+}
+)
+
