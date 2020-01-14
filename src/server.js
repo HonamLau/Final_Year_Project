cedba8c5d27fea024e12406f000322c9027adcb4
HonamLau <hnlau@connect.ust.hk>
2020-01-14 21:58:48 +0800

    test server

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ var http = require("http");
 var app = require('express')();
 var appWs = require('express-ws')(app);
 
-app.ws('/echo', ws => {
-    ws.on('message')
-}
-)
 
+app.get('/',function(request, response){
+    response.send('Hello World')
+}
+);
+
+app.listen(3000,function(){
+    console.log('Sever Listening')
+})
