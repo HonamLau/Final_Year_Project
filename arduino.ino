64bc9ae1fd76e5e88b5183895790a515f422140f
MOK <chmokab@connect.ust.hk>
2020-01-16 12:23:28 +0800

    wifi

--- arduino.ino
+++ arduino.ino
@@ -7,11 +7,12 @@ char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS; 
 char user[] = SECRET_USER;
 
-int status = WL_IDLE_STATUS;     
+int status = WL_IDLE_STATUS;    
 
-String hostName = "www.google.com";
-int pingResult;
 
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "143.89.130.87/";    // name address for Google (using DNS)t pingResult;
+WiFiClient client;
 void setup() {
   
   Serial.begin(9600);
@@ -38,9 +39,23 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Hello");
-  Serial.print("Hellohihi");
+  //Serial.print("Hello");
+  //Serial.print("Hellohihi");
   delay(5000);
+  WebClient();
+
+}
+
+void WebClient(){
+if (client.connect(server, 5000)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    //client.println("GET /search?q=arduino HTTP/1.1");
+    //client.println("Host: www.google.com");
+    //client.println("Connection: close");
+    client.println();
+  }
+
 }
 
 void printWiFiData() {
